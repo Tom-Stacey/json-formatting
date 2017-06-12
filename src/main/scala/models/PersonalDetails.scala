ca246edeb2513e30a7305bce01b56b93fb8241c3
Thomas Stacey <thomas.stacey@digital.hmrc.gov.uk>
2017-06-12 10:48:27 +0100

    Different named fields

--- src/main/scala/models/PersonalDetails.scala
+++ src/main/scala/models/PersonalDetails.scala
@@ -1,6 +1,7 @@
 package models
 
-import play.api.libs.json.Json
+import play.api.libs.functional.syntax._
+import play.api.libs.json._
 
 case class PersonalDetails(
                             firstName: String,
