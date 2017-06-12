cd77f339c7f9209aacb56a623b3c46029a97c80a
Thomas Stacey <thomas.stacey@digital.hmrc.gov.uk>
2017-06-12 11:21:56 +0100

    Multiple field validation

--- src/test/scala/formatters/FormattersSpec.scala
+++ src/test/scala/formatters/FormattersSpec.scala
@@ -2,11 +2,13 @@ package formatters
 
 import java.time.LocalDate
 
+import helpers.JsonValidationTestHelpers
 import models.PersonalDetails
+import play.api.data.validation.ValidationError
 import play.api.libs.json._
 import org.scalatest.{Matchers, WordSpec}
 
-class FormattersSpec extends WordSpec with Matchers {
+class FormattersSpec extends WordSpec with Matchers with JsonValidationTestHelpers {
 
   "Reading basic personal details" should {
     "succeed" in {
@@ -16,6 +18,7 @@ class FormattersSpec extends WordSpec with Matchers {
           |  "name":"Thomas",
           |  "last-name":"Stacey",
           |  "other-name":"Anthony",
+          |  "favoured-wrench":"torque wrench",
           |  "date-of-birth":"2000/03/15"
           |}
         """.stripMargin
@@ -25,10 +28,31 @@ class FormattersSpec extends WordSpec with Matchers {
         firstName = "Thomas",
         middleName = Some("Anthony"),
         surname = "Stacey",
+        starSign = None,
+        favouriteWrench = Some("torque wrench"),
         dob = LocalDate.of(2000,3,15)
       )
 
       Json.fromJson[PersonalDetails](json) shouldBe JsSuccess(deets)
+    }
+
+
+    "fail" when {
+      "Neither star-sign nor wrench preference is provided"in {
+        val json = Json.parse(
+          """
+            |{
+            |  "name":"Thomas",
+            |  "last-name":"Stacey",
+            |  "other-name":"Anthony",
+            |  "date-of-birth":"2000/03/15"
+            |}
+          """.stripMargin
+        )
+
+        val res = Json.fromJson[PersonalDetails](json)
+        shouldHaveErrors(res, JsPath(), Seq(ValidationError("Neither star sign nor preferred wrench provided")))
+      }
     }
   }
 
