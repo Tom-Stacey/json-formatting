a2920af23b10e24e3d62948f5a23eb2290718aab
Thomas Stacey <thomas.stacey@digital.hmrc.gov.uk>
2017-06-12 10:55:08 +0100

    Date parsing

--- src/main/scala/models/PersonalDetails.scala
+++ src/main/scala/models/PersonalDetails.scala
@@ -1,5 +1,7 @@
 package models
 
+import java.time.LocalDate
+
 import play.api.libs.functional.syntax._
 import play.api.libs.json._
 
@@ -7,7 +9,7 @@ case class PersonalDetails(
                             firstName: String,
                             surname: String,
                             middleName: Option[String],
-                            dob: String
+                            dob: LocalDate
                           )
 
 object PersonalDetails {
@@ -15,6 +17,6 @@ object PersonalDetails {
       (__ \ "name").format[String] and
       (__ \ "last-name").format[String] and
       (__ \ "other-name").formatNullable[String] and
-      (__ \ "date-of-birth").format[String]
+      (__ \ "date-of-birth").format[LocalDate]
     )(PersonalDetails.apply, unlift(PersonalDetails.unapply))
 }
